f3fcd639249ef5337c35100da75c1878fd71c069
Sem <Sem.Vaessen@student.gildeopleidingen.nl>
2026-05-20 09:28:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,50 @@
-// const bovenaan (maar 1x!) zodat we ze kunnen gebruiken in alle functies, en niet alleen in de click events
+// knoppen
 const steenBtn = document.querySelector("#steen");
 const papierBtn = document.querySelector("#papier");
 const schaarBtn = document.querySelector("#schaar");
 
+// output
 const computerOutput = document.querySelector("#computer");
 const humanOutput = document.querySelector("#human");
 const resultOutput = document.querySelector("#result");
 
-// begin tekst zodat er iets staat als je de pagina opent, voordat je een keuze hebt gemaakt
+// begintekst
 humanOutput.innerHTML = "Maak je keuze!";
 computerOutput.innerHTML = "-";
 resultOutput.innerHTML = "-";
 
-// variabele die verandert zodat we kunnen zien wat de gebruiker heeft gekozen (steen, papier of schaar)
-let humanChoice = "";
+// computer keuze bepalen
+function getComputerChoice() {
+    const keuzes = ["steen", "papier", "schaar"];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return keuzes[randomIndex];
+}
 
-// click events zodat er iets gebeurt als je klikt op een van de knoppen
-steenBtn.addEventListener("click", function(event) {
-    humanChoice = event.target.id;
-    humanOutput.innerHTML = humanChoice;
-    const randomNumber = Math.floor(Math.random() * 3) + 1;
-    if (randomNumber === 1) {
-        computerOutput.innerHTML = "steen";
-        resultOutput.innerHTML = "gelijkspel!";
-        if (randomNumber === 2) {
-            computerOutput.innerHTML = "papier";
-            resultOutput.innerHTML = "computer wint!";
-        }
-        if (randomNumber === 3) {
-            computerOutput.innerHTML = "schaar";
-            resultOutput.innerHTML = "jij wint!";
-        }
-    }
-});
+// spel logica
+function playGame(humanChoice) {
+    const computerChoice = getComputerChoice();
 
-papierBtn.addEventListener("click", function(event) {
-    humanChoice = event.target.id;
+    // output keuzes
     humanOutput.innerHTML = humanChoice;
-    const randomNumber = Math.floor(Math.random() * 3) + 1;
-    if (randomNumber === 1) {
-        computerOutput.innerHTML = "papier";
+    computerOutput.innerHTML = computerChoice;
+
+    // bepalen winnaar
+    if (humanChoice === computerChoice) {
         resultOutput.innerHTML = "gelijkspel!";
-    }
-    if (randomNumber === 2) {
-        computerOutput.innerHTML = "steen";
+    } 
+    else if (
+        (humanChoice === "steen" && computerChoice === "schaar") ||
+        (humanChoice === "papier" && computerChoice === "steen") ||
+        (humanChoice === "schaar" && computerChoice === "papier")
+    ) {
         resultOutput.innerHTML = "jij wint!";
-    }
-    if (randomNumber === 3) {
-        computerOutput.innerHTML = "schaar";
+    } 
+    else {
         resultOutput.innerHTML = "computer wint!";
     }
-});
+}
 
-schaarBtn.addEventListener("click", function(event) {
-    humanChoice = event.target.id;
-    humanOutput.innerHTML = humanChoice;
-    const randomNumber = Math.floor(Math.random() * 3) + 1;
-    if (randomNumber === 1) {
-        computerOutput.innerHTML = "schaar";
-        resultOutput.innerHTML = "gelijkspel!";
-    }
-    if (randomNumber === 2) {
-        computerOutput.innerHTML = "papier";
-        resultOutput.innerHTML = "jij wint!";
-    }
-    if (randomNumber === 3) {
-        computerOutput.innerHTML = "steen";
-        resultOutput.innerHTML = "computer wint!";
-    }
-});
+// event listeners (allemaal dezelfde functie!)
+steenBtn.addEventListener("click", () => playGame("steen"));
+papierBtn.addEventListener("click", () => playGame("papier"));
+schaarBtn.addEventListener("click", () => playGame("schaar"));
